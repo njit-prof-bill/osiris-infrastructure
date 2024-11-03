11e755accb857bd1e577b3fdc0f31bdefb9c91a2
Haitham Awad <haitham2004xx@gmail.com>
2024-11-03 18:29:19 -0500

    created dummy endpoints api9 api10

--- terraform/app/api/endpoints/state_actions.py
+++ terraform/app/api/endpoints/state_actions.py
@@ -1,3 +1,12 @@
 # 5, 8 9
 from fastapi import APIRouter
 router = APIRouter()
+
+@router.post("/unlock_terraform_state")
+def unlockTerraformState(config_path: str) -> bool:
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)
+
--- terraform/app/api/endpoints/terraform_actions.py
+++ terraform/app/api/endpoints/terraform_actions.py
@@ -3,3 +3,10 @@
 from fastapi import APIRouter
 router = APIRouter()
 
+@router.post("/import_terraform_resource")
+def importTerraformResource(resource_id: str, config_path: str, resource_name: str) -> bool:
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)

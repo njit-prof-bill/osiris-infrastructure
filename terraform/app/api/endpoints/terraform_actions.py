c7df4daa8e05d06f29d688a2b999955023fff0d2
safnoo22 <126367115+safnoo22@users.noreply.github.com>
2024-11-03 18:39:53 -0500

    Update terraform_actions.py

--- terraform/app/api/endpoints/terraform_actions.py
+++ terraform/app/api/endpoints/terraform_actions.py
@@ -1,5 +1,11 @@
-
 # 6, 7, 10
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 router = APIRouter()
 
+@router.post("/validate_terraform")
+def validateTerraform(config_path: str) -> bool:
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)

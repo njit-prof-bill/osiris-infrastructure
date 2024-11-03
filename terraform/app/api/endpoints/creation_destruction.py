66e4a62b5a9b0bfe1baecb352e6acfb19f9e85ee
Ari Kamat <arijkamat@gmail.com>
2024-11-03 15:35:14 -0500

    Created endpoint for API 3 and 4 that just returns true

--- terraform/app/api/endpoints/creation_destruction.py
+++ terraform/app/api/endpoints/creation_destruction.py
@@ -1,3 +1,20 @@
 #1,2,3,4
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 router = APIRouter()
+
+@router.post("/apply_terraform")
+def applyTerraform(config_path: str, var_file: str = None):
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)
+    
+
+@router.post("/destroy_terraform")
+def destroyTerraform(config_path: str, var_file: str = None):
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)

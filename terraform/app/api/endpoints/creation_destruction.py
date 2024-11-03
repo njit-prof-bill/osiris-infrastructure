550d1e8a56e6e68549cdc7a4d2746dd2262cb32a
Haitham Awad <haitham2004xx@gmail.com>
2024-11-03 18:17:52 -0500

    created dummy endpoints api9 api10

--- terraform/app/api/endpoints/creation_destruction.py
+++ terraform/app/api/endpoints/creation_destruction.py
@@ -18,3 +18,21 @@ def destroyTerraform(config_path: str, var_file: str = None):
         return {"success": result}
     except Exception as e:
         raise HTTPException(status_code=500)
+
+
+@router.post("/unlock_terraform_state")
+def unlockTerraformState(config_path: str) -> bool:
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)
+
+
+@router.post("/import_terraform_resource")
+def importTerraformResource(resource_id: str, config_path: str, resource_name: str) -> bool:
+    try:
+        result = True
+        return {"success": result}
+    except Exception as e:
+        raise HTTPException(status_code=500)
